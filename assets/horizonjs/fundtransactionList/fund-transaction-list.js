aea184ba84f0e96fb90c186c373875d8e2e59c01
horizon-dev-group <61450710+horizon-dev-group@users.noreply.github.com>
2020-05-16 15:58:03 +0300

    Update fund-transaction-list.js

--- assets/horizonjs/fundtransactionList/fund-transaction-list.js
+++ assets/horizonjs/fundtransactionList/fund-transaction-list.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
             $.ajax({
                 type: 'GET',
                 contentType: 'application/json',
-                url: 'http://localhost:5000/api/users',
+                url: 'https://evd-server.herokuapp.com/api/users',
                 headers: {
                     'X-Auth-Token': savedToken
                 },
@@ -45,7 +45,7 @@ $(document).ready(function () {
         $.ajax({
             type: 'GET',
             contentType: 'application/json',
-            url: 'http://localhost:5000/api/transaction/',
+            url: 'https://evd-server.herokuapp.com/api/transaction/',
             headers: {
                 'X-Auth-Token': savedToken
             },
